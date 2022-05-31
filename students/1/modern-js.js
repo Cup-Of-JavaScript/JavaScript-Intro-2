512a562435738ea63f2f99b0818e7d6d1c62b2ed
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-05-31 16:05:10 -0400

    update commented out count

--- students/1/modern-js.js
+++ students/1/modern-js.js
@@ -7,7 +7,7 @@
 //
 
 const displayResults = (personList, count) => {
-  let retval = 0;
+  //let count = 0;
   count = personList.length;
   console.table(personList);
   console.log("There are " + count + " people in the list.");
