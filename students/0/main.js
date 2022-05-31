5d951bacfbefe2738ee81a7c7853947b78f684fd
Martin Burolla <mburolla@xpanxion.com>
2022-05-31 07:12:18 -0400

    update

---
+++ students/0/main.js
@@ -0,0 +1,26 @@
+//
+// File: main.js
+// Date: 5/31/2022
+// 
+
+const ex1 = () => {
+   
+}
+
+const ex2 = () => {
+
+}
+
+const ex3 = () => {
+
+}
+
+//
+// Your functions here...
+//
+
+const main = async () => {
+    ex1();
+}
+
+main();
