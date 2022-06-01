66c064c174f2cabba1ddbaf120b83ac78650e4fd
Diane Tewah <dtewah@xpanxion.com>
2022-06-01 11:34:30 -0400

    js code for ex1

--- students/5/main.js
+++ students/5/main.js
@@ -1,26 +1,35 @@
 //
 // File: main.js
 // Date: 5/24/2022
-// 
+//
 
 const ex1 = () => {
-   
-}
+  const array = [1, "2", 3, "test", 1.2];
+  console.log(countNumbers(array));
+};
 
-const ex2 = () => {
+const ex2 = () => {};
 
-}
-
-const ex3 = () => {
-
-}
+const ex3 = () => {};
 
 //
 // Your functions here...
-//
+
+//exercise 1
+const countNumbers = (array) => { 
+    let number = 0;
+ for (let i of array) {
+    if (typeof i === "number") {
+      if (!Number.isInteger(i)) {
+        number++;
+      }
+    }
+  }
+  return number;
+};
 
 const main = async () => {
-    ex1();
-}
+  ex1();
+};
 
 main();
