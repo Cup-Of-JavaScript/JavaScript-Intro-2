bdf735ccbc6975a168cab4bd5d422a641aa8d9b8
Joel Sipin <jsipin@xpanxion.com>
2022-06-01 12:34:19 -0400

    ex1 code

--- students/6/main.js
+++ students/6/main.js
@@ -4,7 +4,7 @@
 // 
 
 const ex1 = () => {
-const array = [1, '2', 3, 'test', 1.2];
+const array = [1, '2', 3, 'test', 1.2, 2.2, 4, '2.3'];
 console.log(countNumbers(array));
 }
 
@@ -20,7 +20,17 @@ const ex3 = () => {
 // Your functions here...
 //
 
-const main = async (countNumbers) => {
+const countNumbers = (array) => {
+    let retval = 0;
+    for (let i of array){
+        if(typeof(i) == 'number' && !Number.isInteger(i)){
+            retval++; 
+        }
+    }
+    return retval;
+}
+
+const main = async() => {
     ex1();
 }
 
