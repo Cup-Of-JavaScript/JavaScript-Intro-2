46b5664029f4554b7b877c9dc9f1d03c88261578
Diane Tewah <dtewah@xpanxion.com>
2022-06-01 16:36:13 -0400

    Ex 2: Min Number

--- students/5/main.js
+++ students/5/main.js
@@ -4,32 +4,50 @@
 //
 
 const ex1 = () => {
-  const array = [1, "2", 3, "test", 1.2];
-  console.log(countNumbers(array));
+    const array = [1, "2", 3, "test", 1.2];
+    console.log(countNumbers(array));
 };
 
-const ex2 = () => {};
+const ex2 = () => {
+    const array = [12, 55, "2", 22, 11, true];
+    console.log(minNumber(array));
+};
 
-const ex3 = () => {};
+const ex3 = () => { };
 
 //
 // Your functions here...
 
 //exercise 1
-const countNumbers = (array) => { 
+const countNumbers = (array) => {
     let number = 0;
- for (let i of array) {
-    if (typeof i === "number") {
-      if (!Number.isInteger(i)) {
-        number++;
-      }
+    for (let i of array) {
+        if (typeof i === "number") {
+            if (!Number.isInteger(i)) {
+                number++;
+            }
+        }
     }
-  }
-  return number;
+    return number;
 };
 
+//exercise 2
+const minNumber = (array) => {
+    let i = 0;
+    let smallestNumber = array[0] // const array = [12, 55, "2", 22, 11, true];
+    for (let i of array) {
+        if (Number.isInteger(i) && i < smallestNumber) { // && means and.
+            smallestNumber = i
+        }
+    }
+    return smallestNumber
+}
+
+
+
+
 const main = async () => {
-  ex1();
+    ex2();
 };
 
 main();
