70970b0b130b9ae19b4d70f7caa308b46b35ed00
progammingwithalanfernandez301@gmail.ccom <afernandez@xpanxion.com>
2022-06-01 11:00:02 -0400

    ex1

--- students/2/main.js
+++ students/2/main.js
@@ -4,13 +4,13 @@
 // 
 
 const ex1 = () => {
-   
+    const array = [1, '2', 3, 'test', 1.2];
+    console.log(countNumbers(array));
 }
-
 const ex2 = () => {
-
+    const array = [12, 55, "2", 22, 11, true];
+    console.log(minNumber(array));
 }
-
 const ex3 = () => {
 
 }
@@ -18,6 +18,18 @@ const ex3 = () => {
 //
 // Your functions here...
 //
+const countNumbers = (array) => {
+    let count = 0
+    for (let i of array) {
+        if (typeof (i) == 'number') {
+            if (!Number.isInteger(i) ) {
+                count++
+            }
+        }
+    }
+    return count;
+}
+
 
 const main = async () => {
     ex1();
