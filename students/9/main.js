c036d44ef0153e8cfe98817ec46153af318c1aa1
steven davis <sdavis@xpanxion.com>
2022-06-01 13:45:11 -0700

    update

--- students/9/main.js
+++ students/9/main.js
@@ -1,29 +1,46 @@
 //
 // File: main.js
 // Date: 5/24/2022
-// 
+//
+
 const ex1 = () => {
-const array = [1, '2', 3, 'test', 1.2];
-console.log(countNumbers(array));
-}
+  const array = [1, "2", 3, "test", 1.2];
+  console.log(countNumbers(array));
+};
+
 const ex2 = () => {
-    const array = [12, 55, "2", 22, 11, true];
-    console.log(minNumber(array)); 
-}
+  const array = [12, 55, "2", 22, 11, true];
+  console.log(minNumber(array));
+};
+
 //
 // Your functions here...
 //
-const minNumber = (array) => {
-    let lowestval = array[0];
-    for(let i of array){ 
-     if (typeof(i) == "number"&& i<lowestval) {
-           lowestval= i
-        }
-     } return lowestval;
+
+const countNumbers = (array) => {
+  let retval = 0;
+  for (let i of array) {
+    if (typeof i == "number") {
+      if (!Number.isInteger(i)) {
+        retval++;
+      }
     }
+  }
+  return retval;
+};
+
+const minNumber = (array) => {
+  let lowestval = array[0];
+  for (let i of array) {
+    if (typeof i == "number" && i < lowestval) {
+      lowestval = i;
+    }
+  }
+  return lowestval;
+};
 
 const main = async () => {
-    ex2();
-}
+  ex2();
+};
 
 main();
