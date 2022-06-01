cacad7b06d8c3de4c55926e032668f7f8f8f7c00
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-06-01 09:44:14 -0400

    Getting Started

--- students/8/main.js
+++ students/8/main.js
@@ -3,8 +3,9 @@
 // Date: 5/24/2022
 // 
 
-const ex1 = () => {
-   
+const ex1 = (array) => {
+    const array = [1, '2', 3, 'test', 1.2];
+    console.log(countNumbers(array));
 }
 
 const ex2 = () => {
@@ -18,6 +19,19 @@ const ex3 = () => {
 //
 // Your functions here...
 //
+
+//Ex 1
+
+const countNumbers = (array) => {
+    let retval = 0;
+    for (let i of array) {
+      if (typeof(i) =='number') {
+        retval++;
+      }
+    } 
+    return retval;
+}
+
 
 const main = async () => {
     ex1();
