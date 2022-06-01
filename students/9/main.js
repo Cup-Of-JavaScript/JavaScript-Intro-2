3c6b266998612680bad6aa61fc58bde17e504794
steven davis <sdavis@xpanxion.com>
2022-06-01 12:42:40 -0700

    update

--- students/9/main.js
+++ students/9/main.js
@@ -2,42 +2,27 @@
 // File: main.js
 // Date: 5/24/2022
 // 
-     
-const ex1 = () => {
-    const array = [1, '2', 3, 'test', 1.2];
-    console.log(countNumbers(array)); 
-}
 
 const ex2 = () => {
-
-}
-
-const ex3 = () => {
-
+    const array = [12, 55, "2", 22, 11, true];
+    console.log(minNumber(array)); 
 }
 
 //
 // Your functions here...
 //
 
-const countNumbers = (array) => {
-    let retval = 0;
-    for(let i of array) {
-         if(typeof(i)=='number') {
-            if(!Number.isInteger(i) ) { 
-                retval++;
-               }    
-            }
-
+const minNumber = (array) => {
+    let lowestval = array[0];
+    for(let i of array){ 
+     if (typeof(i) == "number"&& i<lowestval) {
+           lowestval= i
         }
-        return retval;
+     } return lowestval;
     }
 
 const main = async () => {
-    ex1();
+    ex2();
 }
 
 main();
-
-
-
