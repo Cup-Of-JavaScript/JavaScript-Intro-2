291d4324a9c25d6b0e22312b0446f157eae74cc4
steven davis <sdavis@xpanxion.com>
2022-06-02 13:55:39 -0700

    update

--- students/9/main.js
+++ students/9/main.js
@@ -6,22 +6,23 @@
   const ex1 = () => {
     const array = [1, "2", 3, "test", 1.2];
     console.log(countNumbers(array));
-  };
+  }
 
   const ex2 = () => {
     const array = [12, 55, "2", 22, 11, true];
     console.log(minNumber(array));
-  };
+  }
 
   const ex3 = () => {
     const array = ['this', 'is', 'a', 'test', 'happy'];
     console.log(shortestString(array));
-  };
+  }
 
   const ex4 = () => {
     let array = ["this", "is", "a", "test"];
    console.log(countLetters(array));
-  };
+  }
+
 
 //
 // Your functions here...
@@ -37,31 +38,33 @@ const countNumbers = (array) => {
     }
   }
   return retval;
-};
+}
 
-const minNumber = (array) => {
-  let lowestval = array[0];
-  for (let i of array) {
+  const minNumber = (array) => {
+    let lowestval = array[0];
+    for (let i of array) {
     if (typeof i == "number" && i < lowestval) {
       lowestval = i;
+      }
     }
+    return lowestval;
   }
-  return lowestval;
-};
      
-const shortestString= (array) => {
-   let retval= array.sort((a,b)=> b.length- a.length);
-   return retval[array.length- 1]
-};
+  const shortestString= (array) => {
+    let retval= array.sort((a,b)=> b.length- a.length);
+    return retval[array.length- 1]
+  }
 
-const countLetters = (array) => {
+  const countLetters = (array) => {
     let letterCount = 0
-    for (let i = 0; i < 4; i++) {
-     letterCount += array[i].length
-      }  return letterCount
+    for (let i = 0; i < array.length; i++) {
+      letterCount += array[i].length
+    }  return letterCount
     
-    
-};
+  }   
+
+
+
 const main = async () => {
   ex4();
 }
