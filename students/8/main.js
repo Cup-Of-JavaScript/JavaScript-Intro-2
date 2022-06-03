00a07665b90334077cca78582a4e70057dc8fe21
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-06-03 16:55:50 -0400

    Ex6 Count Types

--- students/8/main.js
+++ students/8/main.js
@@ -31,37 +31,98 @@ const ex5 = () => {
 
 const ex6 = () => {
     let array = ["this", "is", 1, 3, 2.1, "a", "test"];
-    let result = countIt(array);
-    console.log("Number of Integers: " + result.numOfIntegers + "\n" + "Number of Decimals: " + result.numOfDecs + "\n" + "Number of Strings: " + result.numOfStrings)
+    let result = countItems(array);
+    console.log("Number of Integers: " + result.numIntegers)
+    console.log("Number of Decimal Numbers: " + result.numDecimals)
+    console.log("Number of Strings: " + result.numStrings)
+    //console.log("Number of Integers: " + result.numOfIntegers + "\n" + "Number of Decimals: " + result.numOfDecs + "\n" + "Number of Strings: " + result.numOfStrings)
+}
+
+const ex7 = () => {
+    const peopleArray =[];
+    console.log(makepeople(peopleArray));
 }
 //
 // Your functions here...
 //
 
+//Ex 7
+
+// const makepeople = (peopleArray) => {
+//     let person1 = { name: "Alice", age: 22 }
+//     let person2 = { name: "Bob", age: 21 }
+//     let person3 = { name: "Charlie", age: 23 }
+ 
+//     peopleArray.push(person1);
+//     peopleArray.push(person2);
+//     peopleArray.push(person3);
+
+
+    // const getData = (tableName)  => {
+    //     let retval = "No Data";
+    //     if (tableName == "person") {
+    //       retval = [
+    //         { firstName: "Alice", lastName: "Jones", age: 10 },
+    //         { firstName: "Bob", lastName: "Smith", age: 7 },
+    //         { firstName: "Charlie", lastName: "Thumb", age: 5 },
+    //       ];
+    //     }
+
+
+
+// }
+// const main = () => {     //For making the table and sorting alpha
+//     let table = "person";
+//     let personList = getData(table);
+//     personList.sort((a,b) => (a-b));
+//     displayResults(personList);
+//   }
+
 //Ex 6
 
-const countIt = array => {
-    let CountInt = 0;
-    let CountDec = 0;
-    let CountStr = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (typeof (array[i]) == "number" && Number.isInteger(array[i])) {            //Integers
-            CountInt++;
-        }
-        if (typeof (array[i]) == "number" && !Number.isInteger(array[i])) {            //Decimals
-            CountDec++;
-        }
-        if (typeof (array[i]) == "string") {      //Strings
-            CountStr++;
-        }
-    }
 
-    return {
-        numOfIntegers: CountInt,
-        numOfDecs: CountDec,
-        numOfStrings: CountStr
+const countItems = array => {
+    let retval = {
+        numStrings: 0, 
+        numIntegers: 0,
+        numDecimals: 0
     }
+    retval.numStrings =  countStrings(array);
+    retval.numIntegers = countIntegers(array);
+    retval.numDecimals = countDecimals(array);
+    return retval;
 }
+
+const countIntegers = array => {
+    let numIntegers = 0;
+    array.forEach((element) => {
+        if (typeof element === "number" && Number.isInteger(element)) {
+          numIntegers++;
+        }
+    });
+    return numIntegers;
+}
+
+const countDecimals = array => {
+    let numDecimals = 0;
+    array.forEach((element) => {
+        if (typeof element === "number" && !Number.isInteger(element)) {            //Decimals
+        numDecimals++;
+        }
+    });
+    return numDecimals;
+}
+
+const countStrings = array => {
+    let numStrings = 0;
+    array.forEach((element) => {
+        if (typeof (element) == "string") {      //Strings
+        numStrings++;
+        }
+    });  
+    return numStrings;
+}
+
 
 //Ex 5
 
