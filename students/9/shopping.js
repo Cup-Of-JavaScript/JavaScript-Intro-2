c1fb2690e2a4094203d17af3ae0f4bcf339092c4
steven davis <sdavis@xpanxion.com>
2022-06-06 08:44:18 -0700

    update

---
+++ students/9/shopping.js
@@ -0,0 +1,30 @@
+const calculateTotal = (
+  shoppingCartItems = [],
+  shoppingCartQuantities = []
+) => {
+
+  const arr1 = shoppingCartItems;
+  const arr2 = shoppingCartQuantities;
+  let sum = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    const product = arr1[i] * arr2[i];
+    sum += product;
+  }
+  return sum;
+};
+
+const formatCurrency = (dollarAmount) => {
+  let formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  return formatter.format(dollarAmount);
+};
+
+const main = async () => {
+  const shoppingCartItems = [1.0, 2.0, 3.0];
+  const shoppingCartQuantities = [2, 3, 1];
+  console.log(formatCurrency(calculateTotal(shoppingCartItems, shoppingCartQuantities)));
+};
+
+main();
